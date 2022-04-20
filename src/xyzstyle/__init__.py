31c028fe12ad3e99521b761e28492b64a2b2c27e
xinetzone <xinzone@outlook.com>
2022-04-20 20:16:12 +0800

    modified:   pyproject.toml

--- src/xyzstyle/__init__.py
+++ src/xyzstyle/__init__.py
@@ -1,5 +1,10 @@
 """Sphinx demo."""
 from pathlib import Path
+import sys
+
+if sys.platform == 'win32':
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 __version__ = '0.0.2'
 __version_full__ = __version__
