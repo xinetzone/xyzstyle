a0f7c9aff2182ae406ed1fa362b3894f19917495
liuxinwei <liuxinwei@xmsilicon.cn>
2024-11-07 11:01:46 +0800

    更新文档

--- doc/conf.py
+++ doc/conf.py
@@ -8,6 +8,7 @@ from sphinx.application import Sphinx
 
 ROOT = Path('__file__').resolve().parents[1]
 sys.path.extend([str(Path(".").resolve()), str(ROOT/'src')])
+from utils.icon import icon_links
 
 # -- Project information -----------------------------------------------------
 
@@ -181,6 +182,7 @@ html_theme_options = {
         "json_url": json_url,
         "version_match": version_match,
     },
+    "icon_links": icon_links,
     "collapse_navigation": True,
     # "navbar_start": ["test.html"],
     # "navbar_center": ["test.html"],
@@ -228,69 +230,6 @@ html_sidebars = {
 #         # "jupyterhub_url": "https://datahub.berkeley.edu",  # For testing
 #     },
 # }
-
-html_theme_options["icon_links"] = [
-    # {
-    #     "name": "GitHub",
-    #     "url": "https://github.com/xinetzone/tvm-book",
-    #     "icon": "fa-brands fa-square-github",
-    #     "type": "fontawesome",
-    # },
-    {
-        "name": "知乎",
-        "url": "https://www.zhihu.com/people/xinetzone",
-        "icon": "fa-brands fa-zhihu",
-        "type": "fontawesome",
-    },
-    {
-        "name": "简书",
-        "url": "https://www.jianshu.com/u/4302480a3e8e",
-        "icon": "fa-solid fa-book",
-        "type": "fontawesome",
-    },
-    {
-        "name": "B站",
-        "url": "https://space.bilibili.com/252192181",
-        "icon": "fa-brands fa-bilibili",
-        "type": "fontawesome",
-    },
-    {
-        "name": "博客园",
-        "url": "https://www.cnblogs.com/q735613050/",
-        "icon": "https://xinetzone.github.io/xinetzone/media/xinetzone.jpg",
-        "type": "url",
-    },
-    {
-        "name": "领英",
-        "url": "https://www.linkedin.com/in/xinet",
-        "icon": "fa-brands fa-linkedin",
-        "type": "fontawesome",
-    },
-    # {
-    #     "name": "GitLab",
-    #     "url": "https://gitlab.com/<your-org>/<your-repo>",
-    #     "icon": "fa-brands fa-square-gitlab",
-    #     "type": "fontawesome",
-    # },
-    # {
-    #     "name": "Twitter",
-    #     "url": "https://twitter.com/<your-handle>",
-    #     "icon": "fa-brands fa-square-twitter",
-    #     # The default for `type` is `fontawesome` so it is not actually required in any of the above examples as it is shown here
-    # },
-    # {
-    #     "name": "Mastodon",
-    #     "url": "https://<your-host>@<your-handle>",
-    #     "icon": "fa-brands fa-mastodon",
-    # },
-    # {
-    #     "name": "PyData",
-    #     "url": "https://pydata.org",
-    #     "icon": "_static/pydata-logo.png",
-    #     "type": "local",
-    #     "attributes": {"target": "_blank"},
-    # },
-]
 
 def setup(app: Sphinx) -> dict[str, Any]:
     """Add custom configuration to sphinx app.
