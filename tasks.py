589aaa9bb0b8a8fbd7a431440338854a0f3cd1e9
liuxinwei <liuxinwei@xmsilicon.cn>
2024-12-19 16:56:29 +0800

    修改：     tasks.py

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ import inspect
 if not hasattr(inspect, 'getargspec'): # 修复
     inspect.getargspec = inspect.getfullargspec
     
-from d2py.tools.write import site
+from tao.tools.write import site
 
 namespace = site('doc', target='doc/_build/html')
